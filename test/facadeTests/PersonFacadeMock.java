e30dfb58bb61a3611a0a05646dca4961b1b6b648
ChristopherMortensen <christophermortensen@Christophers-MacBook-Pro.local>
2014-09-30 14:09:33 +0200

    Updated PersonFacadeMock.java
    - Added functionality for all methods
    - Created the collection of Persons
    
    Commit: Thomas & Christopher 

--- test/facadeTests/PersonFacadeMock.java
+++ test/facadeTests/PersonFacadeMock.java
@@ -3,10 +3,12 @@ package facadeTests;
 import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.List;
+import model.AssistentTeacher;
 import model.Person;
 import model.RoleSchool;
+import model.Student;
+import model.Teacher;
 import webinterfaces.FacadeInterface;
-
 
 /**
  *
@@ -15,34 +17,64 @@ import webinterfaces.FacadeInterface;
 public class PersonFacadeMock implements FacadeInterface {
 
     private List<Person> persons;
+    private Gson trans;
 
     public PersonFacadeMock() {
         this.persons = new ArrayList<>();
+        this.trans = new Gson();
     }
-    
+
     @Override
     public String getPersonsAsJSON() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return trans.toJson(persons);
     }
 
     @Override
     public String getOnePersonAsJson(long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (Person person : persons) {
+            if (person.getId() == id) {
+                return trans.toJson(person);
+            }
+        }
+        return null;
     }
 
     @Override
     public Person addPersonFromGson(String json) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Person personToAdd = trans.fromJson(json, Person.class);
+        personToAdd.setId(100000l + (persons.size() + 1));
+        persons.add(personToAdd);
+        return personToAdd;
     }
 
     @Override
     public RoleSchool addRoleSchool(String json, long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        RoleSchool role = null;
+        if (json.contains("Teacher")) {
+            role = trans.fromJson(json, Teacher.class);
+        }
+        if (json.contains("Student")) {
+            role = trans.fromJson(json, Student.class);
+        }
+        if (json.contains("AssistentTeacher")){
+            role = trans.fromJson(json, AssistentTeacher.class);
+        }
+        for (Person person : persons) {
+            if (person.getId() == id && role != null) {
+                person.addRole(role);
+            }
+        }
+        return role;
     }
 
     @Override
     public Person delete(long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for(int i = 0; i < persons.size(); i++){
+            if(persons.get(i).getId() == id){
+                return persons.remove(i);
+            }
+        }
+        return null;
     }
 
 }
